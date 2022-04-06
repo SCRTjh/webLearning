a31fe6d8aa8279d8c2ff9f890d6952d7c0f68331
tjh <1277662732@qq.com>
2022-04-06 22:21:59 +0800

    modify 220316demo

--- webL/220316demo/js/jq.js
+++ webL/220316demo/js/jq.js
@@ -17,13 +17,40 @@ $(document).ready(function () {
     $("#addDiv").click(function () {
         $("#boxout").append("<div class='col-md-6' id='box'>追加</div>");
     });
+
+    function checkEmail(value) {
+        return /^([a-zA-Z]|[0-9])(\w|\-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/.test(value);
+    }
+
+    function checkPhone(value) {
+        return /^1[34578][0-9]{9}/i.test(value);
+    }
+
+
+
     $("#email1").change(function () {      //邮箱验证
         $("#fb").removeClass("glyphicon-ok").removeClass("glyphicon-remove");
-        let reg = /^([a-zA-Z]|[0-9])(\w|\-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/;
-        if (reg.test($("#email1").val())) {
+        $('#checkEmail').removeClass('has-success').removeClass('has-error');
+        // let reg = /^([a-zA-Z]|[0-9])(\w|\-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/;
+        if (checkEmail($("#email1").val())) {
             $("#fb").toggleClass("glyphicon-ok");
+            $('#checkEmail').addClass('has-success');
         } else {
             $("#fb").toggleClass("glyphicon-remove");
+            $('#checkEmail').addClass('has-error');
+        }
+    });
+
+    $("#pho1").change(function () {      //电话验证
+        $("#pfb").removeClass("glyphicon-ok").removeClass("glyphicon-remove");
+        $('#checkPho').removeClass('has-success').removeClass('has-error');
+        // let reg = /^([a-zA-Z]|[0-9])(\w|\-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/;
+        if (checkPhone($("#pho1").val())) {
+            $("#pfb").toggleClass("glyphicon-ok");
+            $('#checkPho').addClass('has-success');
+        } else {
+            $("#pfb").toggleClass("glyphicon-remove");
+            $('#checkPho').addClass('has-error');
         }
     });
 });
